dcbab30d6d485c3145807c3328138636d23f42c5
☁️☁️☁️☁️☁️☁️ <106909332+starMinK@users.noreply.github.com>
2022-12-10 15:25:23 +0900

    Delete src/main/java/com/sparta/hanghaestartproject directory

--- src/main/java/com/sparta/hanghaestartproject/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.hanghaestartproject.repository;
-
-import com.sparta.hanghaestartproject.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
