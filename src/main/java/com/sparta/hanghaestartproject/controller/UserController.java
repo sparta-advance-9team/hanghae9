09b8e5a6034d07403bbf06035330100662146fff
starMinK <rbals040329@gmail.com>
2022-12-12 21:42:28 +0900

    어노테이션 제거
    User - @Getter, @NoArgsConstructer
    UserController - @RequiredArgsConstructor
    UserService - @RequiredArgsConstructor
    제거 완

--- src/main/java/com/sparta/hanghaestartproject/controller/UserController.java
+++ src/main/java/com/sparta/hanghaestartproject/controller/UserController.java
@@ -2,7 +2,6 @@ package com.sparta.hanghaestartproject.controller;
 
 import com.sparta.hanghaestartproject.dto.*;
 import com.sparta.hanghaestartproject.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -10,11 +9,15 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping ("/api/user")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor의 역할.. final이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동으로 생성해준다. 17~20줄 참조
 public class UserController {
      
      private final UserService userService;
-     
+
+     public UserController(UserService userService) {
+          this.userService = userService;
+     }
+
      @PostMapping ("/signup")
      public CompleteResponseDto signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
           return userService.signup(signupRequestDto);
