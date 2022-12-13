47fad36ebae4a0d915548afef23783639eb84ab3
jongyeol2 <114922306+jongyeol2@users.noreply.github.com>
2022-12-14 02:39:33 +0900

    좋아요 기능구현
    갯수세기는 X

--- src/main/java/com/sparta/hanghaestartproject/controller/LikeController.java
+++ src/main/java/com/sparta/hanghaestartproject/controller/LikeController.java
@@ -4,6 +4,7 @@ import com.sparta.hanghaestartproject.dto.CompleteResponseDto;
 import com.sparta.hanghaestartproject.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,15 @@ import javax.servlet.http.HttpServletRequest;
 public class LikeController {
      
      private final LikeService likeService;
+
+     @PostMapping("api/like/post/{id}")  // post id
+     public CompleteResponseDto likePost(@PathVariable Long id,HttpServletRequest request) {
+          return likeService.likePost(id, request);
+     }
+
+     @PostMapping("api/like/comment/{id}")   //comment id
+     public CompleteResponseDto likeComment(@PathVariable Long id, HttpServletRequest request) {
+          return likeService.likeComment(id, request);
+     }
 
 }
--- src/main/java/com/sparta/hanghaestartproject/entity/LikePost.java
+++ src/main/java/com/sparta/hanghaestartproject/entity/LikePost.java
@@ -1,10 +1,14 @@
 package com.sparta.hanghaestartproject.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
+@AllArgsConstructor
+@Data
+@Builder
 @Getter
 @Entity
 @NoArgsConstructor
@@ -13,12 +17,29 @@ public class LikePost extends Timestamped{
      @Id
      @GeneratedValue (strategy = GenerationType.AUTO)
      private Long id;
-     
-     @Column (nullable = false)
-     private String username;
+
+     @Column(nullable = false)
+     private boolean status;        //true : 따봉,  false : 따봉취소
+//     @Column (nullable = false)
+//     private String username;
+
+     @ManyToOne(fetch = FetchType.LAZY)
+     @JoinColumn(name = "user_id", nullable = false)
+     @OnDelete(action = OnDeleteAction.CASCADE)  // 유저가 지워지면 유저-게시글 간테이블 삭제
+     private User user;
      
      @ManyToOne(fetch = FetchType.LAZY)
-     @JoinColumn(name="post_id")
+     @JoinColumn(name="post_id", nullable = false)
      private Post post;
-     
+
+     public LikePost(Post post, User user) {
+          this.post = post;
+          this.user = user;
+          this.status = true;
+     }
+
+     public void unLikePost(Post post) {
+          this.status = false;
+//          post.setLiked(post.getLiked() -1);           //좋아요 개수 - 추후에 수정
+     }
 }
